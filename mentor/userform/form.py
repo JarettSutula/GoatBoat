48ec8d3c5e4f619136e5a42db2964a827627d55b
Michael Nielsen <michaelnielsen260@gmail.com>
2021-10-14 02:06:05 -0400

    created new app called userform. created routing and new form to take in info for the user

---
+++ mentor/userform/form.py
@@ -0,0 +1,75 @@
+from django import forms
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from utils import start_db
+db_handle, users = start_db()
+
+
+CLASS_CHOICES = [
+    ('cmpt120', 'CMPT 120'),
+    ('cmpt220', 'CMPT 220'),
+    ('cmpt221', 'CMPT 221'),
+    ('cmpt230', 'CMPT 230'),
+    ('cmpt305', 'CMPT 305'),
+    ('cmpt306', 'CMPT 306'),
+    ('cmpt307', 'CMPT 307'),
+    ('cmpt308', 'CMPT 308'),
+    ('cmpt330', 'CMPT 330'),
+    ('cmpt422', 'CMPT 422'),
+    ('cmpt435', 'CMPT 435'),
+    ('math210', 'MATH 210'),
+    ('math241', 'MATH 241'),
+    ('math242', 'MATH 242'),
+    ('math310', 'MATH 310'),
+    ('math321', 'MATH 321'),
+    ('math331', 'MATH 331'),
+    ('math343', 'MATH 343'),
+    ('math393', 'MATH 393'),
+    ('math394', 'MATH 394'),
+    ]
+
+class UserForm(forms.Form):
+    username = forms.CharField(max_length=100, label='User Name') 
+    firstname = forms.CharField(max_length=100, label='First Name')
+    lastname = forms.CharField(max_length=100, label='Last Name')
+    email = forms.EmailField(required=False, label='Your Email Address')
+    profession = forms.CharField(max_length=100, label='Profession')
+    major = forms.CharField(max_length=100, label='Major')
+    mentorclasschoice= forms.CharField(label='What class are you looking to tutor for?', widget=forms.Select(choices=CLASS_CHOICES))
+    menteeclasschoice= forms.CharField(label='What class are you looking for help in?', widget=forms.Select(choices=CLASS_CHOICES))
+
+def form(request):
+    submitted = False
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            username = form.cleaned_data.get("username")
+            firstname = form.cleaned_data.get("firstname")
+            lastname = form.cleaned_data.get("lastname")
+            email = form.cleaned_data.get("email")
+            profession = form.cleaned_data.get("profession")
+            major = form.cleaned_data.get("major")
+            mentorclasschoice = form.cleaned_data.get("mentorclasschoice")
+            menteeclasschoice = form.cleaned_data.get("menteeclasschoice")
+
+            context= { 'firstname': firstname, 
+                       'lastname':lastname,
+                       'email':email,
+                       'profession':profession,
+                       'major':major,
+                       'mentorclasschoice':mentorclasschoice,
+                       'menteeclasschoice':menteeclasschoice
+                      }
+
+            print(context)
+            
+            users.insert_one(context)
+
+            return HttpResponseRedirect('/form?submitted=True')
+    else:
+        form = UserForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'form.html', {'form': form, 'submitted': submitted})
