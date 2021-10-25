1f34b39366ed8dfab6ce9a4f4bc4a0255e6249f7
Michael Nielsen <michaelnielsen260@gmail.com>
2021-10-25 19:36:48 -0400

    placed the form code inside of the models. fixed the succes page

--- mentor/userform/form.py
+++ mentor/userform/form.py
@@ -1,48 +1,9 @@
 from django import forms
+from userform.models import UserForm, LogInForm
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from utils import start_db
 db_handle, users = start_db()
-
-
-CLASS_CHOICES = [
-    ('none', ''),   
-    ('cmpt120', 'CMPT 120'),
-    ('cmpt220', 'CMPT 220'),
-    ('cmpt221', 'CMPT 221'),
-    ('cmpt230', 'CMPT 230'),
-    ('cmpt305', 'CMPT 305'),
-    ('cmpt306', 'CMPT 306'),
-    ('cmpt307', 'CMPT 307'),
-    ('cmpt308', 'CMPT 308'),
-    ('cmpt330', 'CMPT 330'),
-    ('cmpt422', 'CMPT 422'),
-    ('cmpt435', 'CMPT 435'),
-    ('math210', 'MATH 210'),
-    ('math241', 'MATH 241'),
-    ('math242', 'MATH 242'),
-    ('math310', 'MATH 310'),
-    ('math321', 'MATH 321'),
-    ('math331', 'MATH 331'),
-    ('math343', 'MATH 343'),
-    ('math393', 'MATH 393'),
-    ('math394', 'MATH 394'),
-    ]
-
-class UserForm(forms.Form):
-    username = forms.CharField(max_length=100, label='User Name') 
-    firstname = forms.CharField(max_length=100, label='First Name')
-    lastname = forms.CharField(max_length=100, label='Last Name')
-    email = forms.EmailField(required=False, label='Your Email Address')
-    profession = forms.CharField(max_length=100, label='Profession')
-    major = forms.CharField(max_length=100, label='Major')
-    mentorclasschoice= forms.CharField(label='What class are you looking to tutor for?', widget=forms.Select(choices=CLASS_CHOICES))
-    menteeclasschoice= forms.CharField(label='What class are you looking for help in?', widget=forms.Select(choices=CLASS_CHOICES))
-
-class LogInForm(forms.Form):
-    username = forms.CharField(max_length=100, label='User Name')
-    password = forms.CharField(widget=forms.PasswordInput)
-
 
 
 def create_user_form(request):
@@ -74,7 +35,7 @@ def create_user_form(request):
             
             users.insert_one(context)
 
-            return HttpResponseRedirect('/form?submitted=True')
+            return HttpResponseRedirect('/form/createuser?submitted=True')
     else:
         form = UserForm()
         if 'submitted' in request.GET:
@@ -100,7 +61,7 @@ def login_form(request):
 
             users.insert_one(context)
 
-            return HttpResponseRedirect('/form?submitted=True')
+            return HttpResponseRedirect('/form/login?submitted=True')
     else:
         form = LogInForm()
         if 'submitted' in request.GET:
--- mentor/userform/models.py
+++ mentor/userform/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+CLASS_CHOICES = [
+    ('none', ''),
+    ('cmpt120', 'CMPT 120'),
+    ('cmpt220', 'CMPT 220'),
+    ('cmpt221', 'CMPT 221'),
+    ('cmpt230', 'CMPT 230'),
+    ('cmpt305', 'CMPT 305'),
+    ('cmpt306', 'CMPT 306'),
+    ('cmpt307', 'CMPT 307'),
+    ('cmpt308', 'CMPT 308'),
+    ('cmpt330', 'CMPT 330'),
+    ('cmpt422', 'CMPT 422'),
+    ('cmpt435', 'CMPT 435'),
+    ('math210', 'MATH 210'),
+    ('math241', 'MATH 241'),
+    ('math242', 'MATH 242'),
+    ('math310', 'MATH 310'),
+    ('math321', 'MATH 321'),
+    ('math331', 'MATH 331'),
+    ('math343', 'MATH 343'),
+    ('math393', 'MATH 393'),
+    ('math394', 'MATH 394'),
+    ]
+
+class UserForm(forms.Form):
+    username = forms.CharField(max_length=100, label='User Name')
+    firstname = forms.CharField(max_length=100, label='First Name')
+    lastname = forms.CharField(max_length=100, label='Last Name')
+    email = forms.EmailField(required=False, label='Your Email Address')
+    profession = forms.CharField(max_length=100, label='Profession')
+    major = forms.CharField(max_length=100, label='Major')
+    mentorclasschoice= forms.CharField(label='What class are you looking to tutor for?', widget=forms.Select(choices=CLASS_CHOICES))
+    menteeclasschoice= forms.CharField(label='What class are you looking for help in?', widget=forms.Select(choices=CLASS_CHOICES))
+
+class LogInForm(forms.Form):
+    username = forms.CharField(max_length=100, label='User Name')
+    password = forms.CharField(widget=forms.PasswordInput)
