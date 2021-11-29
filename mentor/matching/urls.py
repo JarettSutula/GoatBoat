8825b8ebd17cb610f24bcd47de5373d73f7b38f3
Michael A. Nielsen <michael.nielsen@marist.edu>
2021-11-29 14:57:10 -0500

    created a mentormatch html page where we can have the matching occur. can get the matched mentor but need to render it in the template. also created two test users.

--- mentor/matching/urls.py
+++ mentor/matching/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import matchPageView, ClassChoiceFormPageView, MentorFormPageView
+from .views import matchPageView, ClassChoiceFormPageView, MentorFormPageView, MentorMatchingPageView
 
 urlpatterns = [
     path('', matchPageView, name='match'),
     path('mentorform/', MentorFormPageView, name='mentorMatch'),
     path('chooseclass/', ClassChoiceFormPageView, name='classChoiceForm'),
+    path('mentormatching/', MentorMatchingPageView, name='mentorMatching'),
 ]
